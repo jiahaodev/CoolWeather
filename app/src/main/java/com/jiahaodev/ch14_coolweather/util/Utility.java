048415390a81943e098697f3aaeefc89db6bcaa8
Jiahao <jiahaodev@163.com>
2017-02-12 11:51:26 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/jiahaodev/ch14_coolweather/util/Utility.java
@@ -0,0 +1,99 @@
+package com.jiahaodev.ch14_coolweather.util;
+
+import android.text.TextUtils;
+
+import com.jiahaodev.ch14_coolweather.db.City;
+import com.jiahaodev.ch14_coolweather.db.County;
+import com.jiahaodev.ch14_coolweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by WilliamWu on 2017/2/12.
+ * 功能：将返回的json信息解析后，存储到数据库
+ *
+ */
+
+public class Utility {
+
+    /**
+     * 解析和处理服务器返回的省级数据
+     * @param response 省级数据
+     * @return
+     */
+    public static boolean handleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0; i <allProvinces.length() ; i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的市级数据
+     * @param response   市级数据
+     * @param provinceId 当前市所属的省的id
+     * @return
+     */
+    public static boolean handleCityResponse(String response,int provinceId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i <allCities.length() ; i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的市级数据
+     * @param response   区（县）级数据
+     * @param cityId     当前区（县）所属的市的id
+     * @return
+     */
+    public static boolean handleCountyResponse(String response,int cityId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0; i <allCounties.length() ; i++) {
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherCode(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+
+}
